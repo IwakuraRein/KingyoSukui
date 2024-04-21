9e31bde63e5db9cd6ced47b041dc5b191354f22b
IwakuraRein <28486541+IwakuraRein@users.noreply.github.com>
2024-04-21 17:33:05 -0400

    Poi get wet

--- Assets/Scripts/Poi.cs
+++ Assets/Scripts/Poi.cs
@@ -7,13 +7,64 @@ namespace Kingyo
 {
     public class Poi : MonoBehaviour
     {
+        [SerializeField]
+        Renderer render;
+        [SerializeField]
+        float timeGetWet = 1f;
+        [SerializeField]
+        float timeGetDry = 3f;
+
+        bool isInWater = false;
+        Material mat;
+        public bool IsInWater { get => isInWater; set => isInWater = value; }
+
         public UltEvent<Poi, Fish> OnFishEnterPoi = new UltEvent<Poi, Fish>();
         public UltEvent<Poi, Fish> OnFishExitPoi = new UltEvent<Poi, Fish>();
-        void Start()
+        public UltEvent<Poi> OnPoiEnterWater = new UltEvent<Poi>();
+        public UltEvent<Poi> OnPoiExitWater = new UltEvent<Poi>();
+
+        private void Start()
         {
-
+            mat = render.material;
+            OnPoiEnterWater += (Poi _) =>
+            {
+                StopAllCoroutines();
+                StartCoroutine(GetWet());
+            };
+            OnPoiExitWater += (Poi _) =>
+            {
+                StopAllCoroutines();
+                StartCoroutine(GetDry());
+            };
         }
+        IEnumerator GetWet()
+        {
+            float wetness = mat.GetFloat("_Wetness");
+            float elapsedTime = wetness * timeGetWet;
 
+            while (wetness < 0.99999f)
+            {
+                float t = Mathf.Clamp01(elapsedTime / timeGetWet);
+                mat.SetFloat("_Wetness", t);
+
+                elapsedTime += 0.025f;
+                yield return new WaitForSeconds(0.025f);
+            }
+        }
+        IEnumerator GetDry()
+        {
+            float wetness = mat.GetFloat("_Wetness");
+            float elapsedTime = (1f - wetness) * timeGetDry;
+
+            while (wetness > Mathf.Epsilon)
+            {
+                float t = 1f - Mathf.Clamp01(elapsedTime / timeGetDry);
+                mat.SetFloat("_Wetness", t);
+
+                elapsedTime += 0.025f;
+                yield return new WaitForSeconds(0.025f);
+            }
+        }
         void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Hands"))
@@ -35,7 +86,7 @@ namespace Kingyo
                     GameManager.Instance.hasPoiOnHand = true;
                 }
             }
-            if (other.gameObject.tag == "Fish")
+            if (other.CompareTag("Fish"))
             {
                 var fish = other.attachedRigidbody.gameObject.GetComponent<Fish>();
                 if (fish != null)
@@ -48,10 +99,19 @@ namespace Kingyo
                     }
                 }
             }
+            if (other.CompareTag("Water"))
+            {
+                if (!IsInWater)
+                {
+                    isInWater = true;
+                    OnPoiEnterWater?.Invoke(this);
+                    Debug.Log($"{this} enters water!");
+                }
+            }
         }
         private void OnTriggerExit(Collider other)
         {
-            if (other.gameObject.tag == "Fish")
+            if (other.CompareTag("Fish"))
             {
                 var fish = other.attachedRigidbody.gameObject.GetComponent<Fish>();
                 if (fish != null)
@@ -62,6 +122,15 @@ namespace Kingyo
                         OnFishExitPoi?.Invoke(this, fish);
                         Debug.Log($"{fish} leaves the poi!");
                     }
+                }
+            }
+            if (other.CompareTag("Water"))
+            {
+                if (IsInWater)
+                {
+                    isInWater = false;
+                    OnPoiExitWater?.Invoke(this);
+                    Debug.Log($"{this} leaves water!");
                 }
             }
         }
