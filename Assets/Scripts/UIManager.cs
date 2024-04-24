6f087e723588f4702fbf35f59ed13027a1e0d29c
Scriptwonder <1300285021@qq.com>
2024-04-23 23:07:50 -0400

    MenuChange
    Score now has a goal

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,6 +14,8 @@ namespace Kingyo
         private TextMeshProUGUI levelText;
         [SerializeField]
         private TextMeshProUGUI timeText;
+        [SerializeField]
+        private TextMeshProUGUI goalText;
         // Start is called before the first frame update
         private Bowl bowl;
         private float startingTime;
@@ -24,6 +26,14 @@ namespace Kingyo
             { 2, 150f }, // Level 2 has a time limit of 150 seconds
             { 3, 100f } // Level 3 has a time limit of 100 seconds
             // Add more levels and their time limits as needed
+        };
+
+        private Dictionary<int, int> levelGoals = new Dictionary<int, int>()
+        {
+            { 1, 10 }, // Level 1 has a goal of 100 points
+            { 2, 15 }, // Level 2 has a goal of 200 points
+            { 3, 20 } // Level 3 has a goal of 300 points
+            // Add more levels and their goals as needed
         };
         private void Awake()
         {
@@ -66,11 +76,27 @@ namespace Kingyo
         void UpdateScore(int score)
         {
             scoreText.text = "Score: " + score;
+            if (score >= ((SceneManager.GetActiveScene().buildIndex == 0) ? 10 : levelGoals[SceneManager.GetActiveScene().buildIndex]))
+            {
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                //play some winning scenario
+                if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1)
+                {
+                    // Load the menu scene if this is the last level
+                    SceneManager.LoadScene(0);
+                }
+                else
+                {
+                    // Load the next level
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                }
+            }
         }
 
         void startLevel(int level)
         {
             levelText.text = "Level: " + level;
+            goalText.text = level == 0 ? "Goal: " + levelGoals[1] : "Goal: " + levelGoals[level];
             startingTime = level == 0? levelTimeLimits[1] : levelTimeLimits[level];
         }
 
